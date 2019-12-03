0b34fd6729d9831fe02d32f56398309eac91070f
Rasumusu <56589390+Rasumusu@users.noreply.github.com>
2019-12-03 13:12:04 +0100

    fan

--- src/Controller.java
+++ src/Controller.java
@@ -1,23 +1,23 @@
 import javax.swing.JFrame;
 
 public class Controller {
-	PersonRegister register = new PersonRegister();
+	PersonRegister reg = new PersonRegister();
 	Person p = new Person();
 	Account acc; 
 	JFrame customerFrame;
 
 	public void addPerson(String name, String pNbr, Person p) {
-		register.addPerson(p);
+		reg.addPerson(p);
 		p.setName(name);
 		p.setpNbr(pNbr);
 	}
 
 	public Person removePerson(String pNbr) {
-		return register.removePerson(pNbr);
+		return reg.removePerson(pNbr);
 	}
 	
 	public Person findPerson(String pNbr) {
-		return register.findPerson(pNbr); 
+		return reg.findPerson(pNbr); 
 	}
 	public void addAccount(Account a, Person p, String Nbr){
 		p.harAccount(a);
